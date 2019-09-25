a210f63e4956612f6fc54eb6670419fdd067a8c3
MANIAHMED <42448210+MANIAHMED@users.noreply.github.com>
2019-09-25 20:10:11 +0500

    Update 131A_CapsLOck.py

--- 131A_CapsLOck.py
+++ 131A_CapsLOck.py
@@ -1,1 +1,13 @@
+str = input()
 
+if str.isupper():
+    str=str.lower()
+    print(str)
+elif str[1:].isupper():
+    str=str.swapcase();
+    print(str)
+elif len(str)==1 and str.islower():
+    str=str.swapcase()
+    print(str)
+else:
+    print(str)
